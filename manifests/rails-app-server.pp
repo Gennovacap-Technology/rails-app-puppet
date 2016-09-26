854ff3bda95b031ea73212cb91a46781c31e5c2e
Anderson Brandao <anderson@quimbik.com>
2016-09-26 19:39:28 -0300

    Added databases.

--- manifests/rails-app-server.pp
+++ manifests/rails-app-server.pp
@@ -2,48 +2,49 @@
 
 node 'rails-app-server' {
 
+  #Classes
+
+  hiera_include('classes')
+
   # Hiera lookups
   $packages = hiera_array('packages')
 
   # Validate all the variables
   validate_array($packages)
 
-
   package { $packages:
-     ensure => 'installed',
+    ensure => 'installed',
   }
 
-include '::nginx'
-include '::ntp'
-include 'jenkins'
+  $databases = hiera_hash('databases')
 
-   class { 'postgresql::server': }
+  $databases.each |$key,$value| {
+    postgresql::server::db { $key:
+      user     => $value['user'],
+      password => postgresql_password($value['user'], $value['password']),
+    }
 
-   postgresql::server::db { 'gcap_prod':
-      user     => 'gcapdbuser',
-      password => postgresql_password('gcapdbuser', '6JPP9py4lrt'),
-   }
+  }
 
 
-   class { 'docker' :
-	docker_users => ['jenkins'],
-	tcp_bind     => 'tcp://127.0.0.1:4243',
-        socket_bind  => 'unix:///var/run/docker.sock',
-   }
-
-   nginx::resource::upstream { 'jenkins_upstream':
-     members => [
-       'localhost:8080',
-     ],
-   }
-
-   nginx::resource::vhost { 'ci.gennovacap.com':
-     proxy => 'http://jenkins_upstream',
-     ssl              => true,
-     rewrite_to_https => true,
-     ssl_key          => '/etc/letsencrypt/live/ci.gennovacap.com/privkey.pem',
-     ssl_cert         => '/etc/letsencrypt/live/ci.gennovacap.com/fullchain.pem',
-
-   }
+  # class { 'docker' :
+  #   docker_users => ['jenkins'],
+  #   tcp_bind     => 'tcp://127.0.0.1:4243',
+  #   socket_bind  => 'unix:///var/run/docker.sock',
+  # }
+  #
+  # nginx::resource::upstream { 'jenkins_upstream':
+  #   members => [
+  #   'localhost:8080',
+  #   ],
+  # }
+  #
+  # nginx::resource::vhost { 'ci.gennovacap.com':
+  #   proxy => 'http://jenkins_upstream',
+  #   ssl              => true,
+  #   rewrite_to_https => true,
+  #   ssl_key          => '/etc/letsencrypt/live/ci.gennovacap.com/privkey.pem',
+  #   ssl_cert         => '/etc/letsencrypt/live/ci.gennovacap.com/fullchain.pem',
+  # }
 
 }
